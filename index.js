7289ce04f7b19fbda108ec2593f70698ed278ea9
Edantuti <66787358+Edantuti@users.noreply.github.com>
2020-11-23 14:34:15 +0530

    Added a 2d function to the program

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import {createCanvas, GLInstance, initShaderProgram} from './modules/module.js';
-import {drawScene, initBuffers} from './modules/shader.js';
+import {drawScene, initBuffers, drawScene2d, initBuffers2d} from './modules/shader.js';
 createCanvas('firewall-content');
-var Gl = new GLInstance('display').fClear().fSetSize(1280, 720);
+var Gl = new GLInstance('display').fClear().fSetSize(800, 600);
 
-const indices = [
+const indices3d = [
     0,  1,  2,      0,  2,  3,    // front
     4,  5,  6,      4,  6,  7,    // back
     8,  9,  10,     8,  10, 11,   // top
@@ -12,7 +12,7 @@ const indices = [
     20, 21, 22,     20, 22, 23,   // left
 	];
 
-const color = [
+const color3d = [
     [1.0,  1.0,  1.0,  1.0],    // Front face: white
     [1.0,  0.0,  0.0,  1.0],    // Back face: red
     [0.0,  1.0,  0.0,  1.0],    // Top face: green
@@ -20,7 +20,19 @@ const color = [
     [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
     [1.0,  0.0,  1.0,  1.0],    // Left face: purple
   ];
+const color = [
+    1.0,  1.0,  1.0,  1.0,    // white
+    1.0,  0.0,  0.0,  1.0,    // red
+    0.0,  1.0,  0.0,  1.0,    // green
+    0.0,  0.0,  1.0,  1.0,    // blue
+  ];
 const position = [
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ];
+const position3d = [
     // Front face
     -1.0, -1.0,  1.0,
      1.0, -1.0,  1.0,
@@ -94,8 +106,25 @@ const programInfo = {
         modelViewMatrix: Gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
     },
 };
+//this is for 2d
+// const buffers = initBuffers2d(Gl, position, color);
+// var then = 1;
 
-const buffers = initBuffers(Gl, position, color, indices);
+// function render(now){
+//     now *= 0.001;
+//     const deltaTime = now - then;
+//     then = now;
+
+//     drawScene2d(Gl, programInfo, buffers, deltaTime);
+
+//     requestAnimationFrame(render);
+// }
+
+// requestAnimationFrame(render);
+
+
+// this is for 3d cube;
+const buffers = initBuffers(Gl, position3d, color3d, indices3d);
 var then = 1;
 
 function render(now){
--- modules/shader.js
+++ modules/shader.js
@@ -1,12 +1,111 @@
 var cubeRotation = 0.0;
+var squareRotation = 0.0;
+
+let initBuffers2d = function(gl, position, color){
+	const positionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	const positions = position;
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	const colors = color;
+	const colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	return{
+		position: positionBuffer,
+		color: colorBuffer,
+	}
+}
+
+let drawScene2d = function(gl, programInfo, buffers, deltaTime){
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth/ gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+	const projectionMatrix = mat4.create();
+	const modelViewMatrix = mat4.create();
+
+	mat4.perspective(projectionMatrix, fieldOfView, aspect,zNear,zFar);
+
+    mat4.translate(modelViewMatrix, modelViewMatrix,[-0.0, 0.0, -6.0]);
+										
+	mat4.rotate(modelViewMatrix,modelViewMatrix,squareRotation,[0,0,1]);
+
+
+	{
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+		const offset = 0;
+		
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+		programInfo.attribLocations.vertexPosition);
+		
+	}
+	{
+		const numComponents = 4;
+		const type = gl.FLOAT;
+		const normalize = false;
+		const stride = 0;
+		const offset = 0;
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+		gl.vertexAttribPointer(
+			programInfo.attribLocations.vertexColor,
+			numComponents,
+			type,
+			normalize,
+			stride,
+			offset
+		);
+		gl.enableVertexAttribArray(
+			programInfo.attribLocations.vertexColor
+		);
+	}
+	gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix
+    );
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix
+    );
+    {
+        const offset = 0;
+		const vertexCount = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+		}
+		
+		squareRotation += deltaTime;
+}
+
 let initBuffers = function(gl, position, color, indice){
    const positionBuffer = gl.createBuffer();
    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
    const positions = position;
 
-   gl.bufferData(gl.ARRAY_BUFFER,
-                 new Float32Array(positions),
-								 gl.STATIC_DRAW);
+   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 								 
 
 	 const faceColors = color;
@@ -49,27 +148,15 @@ let drawScene = function(gl, programInfo, buffers, deltaTime){
     const aspect = gl.canvas.clientWidth/ gl.canvas.clientHeight;
     const zNear = 0.1;
     const zFar = 100.0;
-		const projectionMatrix = mat4.create();
-		const modelViewMatrix = mat4.create();
+	const projectionMatrix = mat4.create();
+	const modelViewMatrix = mat4.create();
 
-    mat4.perspective(projectionMatrix, 
-                    fieldOfView,
-                    aspect,
-                    zNear,
-                    zFar);
+    mat4.perspective(projectionMatrix, fieldOfView, aspect,zNear,zFar);
 
-    mat4.translate(modelViewMatrix,
-                    modelViewMatrix,
-					[-0.0, 0.0, -6.0]);
+    mat4.translate(modelViewMatrix, modelViewMatrix,[-0.0, 0.0, -6.0]);
 										
-		mat4.rotate(modelViewMatrix,
-					modelViewMatrix,
-					cubeRotation,
-								[0,1,1]);
-		mat4.rotate(modelViewMatrix,
-					modelViewMatrix,
-					cubeRotation * .7,
-					[0, 1, 0]);
+	mat4.rotate(modelViewMatrix,modelViewMatrix,cubeRotation,[0,1,1]);
+	mat4.rotate(modelViewMatrix,modelViewMatrix,cubeRotation * .7,[0, 1, 0]);
 
     {
         const numComponents = 3;
@@ -133,4 +220,4 @@ let drawScene = function(gl, programInfo, buffers, deltaTime){
 		cubeRotation += deltaTime;
 }
 
-export{drawScene, initBuffers}
+export{drawScene, initBuffers, drawScene2d, initBuffers2d}
